225d9767322c028e3479917945c097ef201c1d92
James Ashcroft <james.ashcroft@renishaw.com>
2023-01-19 22:38:12 +0000

    Button Styling

--- Remoting Wizard/ViewModels/RemotingWizardViewModel.cs
+++ Remoting Wizard/ViewModels/RemotingWizardViewModel.cs
@@ -17,6 +17,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using static Remoting_Wizard.Class.Screen;
 
 namespace Remoting_Wizard.ViewModels
